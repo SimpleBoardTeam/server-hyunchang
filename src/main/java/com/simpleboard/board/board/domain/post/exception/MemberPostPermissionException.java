6de3c753db710fb1f3b62ad6af68d0c5fd6f0abe
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-07-18 16:40:12 +0900

    chore: Apply spotless

--- src/main/java/com/simpleboard/board/board/domain/post/exception/MemberPostPermissionException.java
+++ src/main/java/com/simpleboard/board/board/domain/post/exception/MemberPostPermissionException.java
@@ -3,7 +3,6 @@ package com.simpleboard.board.board.domain.post.exception;
 import com.simpleboard.board.global.exception.ErrorCode;
 import com.simpleboard.board.global.exception.ServiceException;
 
-
 public class MemberPostPermissionException extends ServiceException {
 
   private static final ErrorCode ERROR_CODE = ErrorCode.NO_DELETE_PERMISSION;
