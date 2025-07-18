5e5fe70dd2d51f9249fabd22b514f329cfa7cbce
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-07-18 16:37:14 +0900

    feat(post): PostLike 엔티티 & VO 추가 - PostLike 엔티티 추가 - PostLike 관리 VO 추가

---
+++ src/main/java/com/simpleboard/board/board/domain/post/entity/PostLike.java
@@ -0,0 +1,75 @@
+package com.simpleboard.board.board.domain.post.entity;
+
+import com.simpleboard.board.board.domain.common.vo.Visitor;
+import com.simpleboard.board.board.domain.common.vo.VisitorType;
+import com.simpleboard.board.board.domain.post.vo.PostTypeEnum;
+
+/**
+ * <b>PostLike</b> Entity(non‑root)
+ *
+ * <p>Post에 대한 like 정보
+ *
+ * @domain entity
+ * @since 1.0
+ */
+public class PostLike {
+  String vid;
+  Long likedMemberId;
+
+  private PostLike(String vid, Long memberId) {
+    this.vid = vid;
+    this.likedMemberId = memberId;
+  }
+
+  public static PostLike of(Visitor visitor) {
+    return new PostLike(visitor.vid(), visitor.memberId());
+  }
+
+  /**
+   * <b>좋아요 확인 메서드</b>
+   *
+   * <p>visitor가 해당 PostLike에 대한 생성자인지 체크
+   *
+   * <p>Visitor가 Member이고, Member의 vid가 PostLike의 vid와 같을 경우, PostLike의 memberId에 visitor의 memberId
+   * 추가
+   *
+   * @since 1.0
+   */
+  public boolean isLiker(Visitor visitor, PostTypeEnum postType) {
+
+    // 1. Post가 Member라면 MemberId값만 본다.
+    if (postType == PostTypeEnum.MEMBER) return checkMemberPost(visitor);
+
+    // 2. GuestPost지만 PostLike에 MemberId가 기록되어있을 때
+    if (likedMemberId != null) {
+
+      // 2-1. 방문자가 member일 때
+      if (visitor.type() == VisitorType.MEMBER) {
+        // 2-2. MemberId가 같으면 true
+        if (likedMemberId.equals(visitor.memberId())) return true;
+      }
+      return vid.equals(visitor.vid());
+      // 2-3. MemberId가 달라도 vid가 같으면 true, vid까지 다르면 false
+    }
+
+    // 3. GuestPost이고, PostLike에 MemberId가 없을 때
+    if (vid.equals(visitor.vid())) {
+
+      likedMemberId = visitor.memberId();
+      return true;
+    }
+
+    return false;
+  }
+
+  // MemberPost에서는 무조건 멤버아이디만 체크
+  private boolean checkMemberPost(Visitor visitor) {
+    if (visitor.type() != VisitorType.MEMBER) return false;
+    if (likedMemberId.equals(visitor.memberId())) {
+      vid = visitor.vid();
+      return true;
+    }
+
+    return false;
+  }
+}
---
+++ src/main/java/com/simpleboard/board/board/domain/post/vo/LikesVO.java
@@ -0,0 +1,69 @@
+package com.simpleboard.board.board.domain.post.vo;
+
+import com.simpleboard.board.board.domain.common.vo.Visitor;
+import com.simpleboard.board.board.domain.post.dto.LikeInfo;
+import com.simpleboard.board.board.domain.post.entity.PostLike;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * <b>Likes</b> Value Object.
+ *
+ * <p>Post에 대한 Likes 관련 책임 담당 VO
+ *
+ * @domain value-object
+ * @since 1.0
+ */
+public record LikesVO(List<PostLike> likes) {
+
+  /**
+   * <b>생성 static 메서드.</b>
+   *
+   * <p>빈 list 생성 후 반환
+   *
+   * @since 1.0
+   */
+  public static LikesVO create() {
+    return new LikesVO(List.of());
+  }
+
+  /**
+   * <b>Like 토글 메서드</b>
+   *
+   * <p>Visitor에 해당하는 PostLike가 있다면 삭제, 없다면 생성
+   *
+   * @since 1.0
+   */
+  public LikeInfo toggleLike(Visitor visitor, PostTypeEnum postType) {
+    PostLike postLike = getPostLike(visitor, postType);
+    if (postLike == null) addLike(visitor);
+    else deleteLike(postLike);
+
+    return new LikeInfo(postLike == null, likes.size());
+  }
+
+  /**
+   * <b>visitor에 대한 PostLike 탐색 메서드.</b>
+   *
+   * <p>visitor가 생성한 PostLike가 존재하는지 확인한다.
+   *
+   * @since 1.0
+   */
+  public Boolean isLiked(Visitor visitor, PostTypeEnum postType) {
+    return getPostLike(visitor, postType) != null;
+  }
+
+  private PostLike getPostLike(Visitor visitor, PostTypeEnum postType) {
+    Optional<PostLike> postLike =
+        likes.stream().filter(v -> v.isLiker(visitor, postType)).findFirst();
+    return postLike.orElse(null);
+  }
+
+  private void addLike(Visitor visitor) {
+    likes.add(PostLike.of(visitor));
+  }
+
+  private void deleteLike(PostLike postLike) {
+    likes.remove(postLike);
+  }
+}
