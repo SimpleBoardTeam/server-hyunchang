d241fb910edcd6a692bd24ac77c595353fabe62f
jungsu <vwx1@naver.com>
2025-07-19 16:50:01 +0900

    feat(permission): getter 추가 및 boardId 변수 추가

--- src/main/java/com/simpleboard/board/permission/domain/ManagerAssignment.java
+++ src/main/java/com/simpleboard/board/permission/domain/ManagerAssignment.java
@@ -1,17 +1,22 @@
 package com.simpleboard.board.permission.domain;
 
+import lombok.Getter;
+
+@Getter
 public class ManagerAssignment {
+  private final Long boardId;
 
   private final Long userId;
   private final Role role;
 
-  private ManagerAssignment(Long userId, Role role) {
+  private ManagerAssignment(Long boardId, Long userId, Role role) {
+    this.boardId = boardId;
     this.userId = userId;
     this.role = role;
   }
 
-  public static ManagerAssignment create(Long userId, RoleName roleName) {
-    return new ManagerAssignment(userId, Role.of(roleName));
+  public static ManagerAssignment create(Long boardId, Long userId, RoleName roleName) {
+    return new ManagerAssignment(boardId, userId, Role.of(roleName));
   }
 
   public boolean hasPermission(Permission permission) {
@@ -24,5 +29,9 @@ public class ManagerAssignment {
 
   public boolean isOwnedBy(Long userId) {
     return userId.equals(this.userId);
+  }
+
+  public RoleName getRoleName() {
+    return role.getRoleName();
   }
 }
