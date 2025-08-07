e1dff5487cf86803591908bb2165de39215a87fe
jungsu <vwx1@naver.com>
2025-08-07 14:43:22 +0900

    feat(member): 스프링에서 관리할 수 있도록 @Component 추가

--- src/main/java/com/simpleboard/board/member/domain/NicknameDuplicationChecker.java
+++ src/main/java/com/simpleboard/board/member/domain/NicknameDuplicationChecker.java
@@ -2,7 +2,9 @@ package com.simpleboard.board.member.domain;
 
 import com.simpleboard.board.member.domain.exception.DuplicatedNicknameException;
 import com.simpleboard.board.member.domain.repository.MemberRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NicknameDuplicationChecker {
 
   private final MemberRepository memberRepository;
