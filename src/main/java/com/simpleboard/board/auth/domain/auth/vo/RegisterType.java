a3d2a8437c9430eb838434718d32749d069bff01
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-08-12 20:51:37 +0900

    feat(auth): Authentication aggregate 정의 (#74) - Auth aggregate 공통(abstract)클래스 정의

---
+++ src/main/java/com/simpleboard/board/auth/domain/auth/vo/RegisterType.java
@@ -0,0 +1,16 @@
+package com.simpleboard.board.auth.domain.auth.vo;
+
+/**
+ * <b>회원가입 타입 enum</b> 열거형.
+ *
+ * <p>유저의 회원가입 방법을 표기
+ *
+ * @domain enum
+ * @since 1.0
+ */
+public enum RegisterType {
+  EMAIL,
+  GOOGLE,
+  KAKAO,
+  ;
+}
