7949447cf963e0f83e7b9cf68f70f7036ed7626a
jungsu <vwx1@naver.com>
2025-08-20 16:39:48 +0900

    feat(member): member 등록 추가
    - registerMember 를 통해서 member 등록 추가

--- src/main/java/com/simpleboard/board/member/application/command/MemberCommandService.java
+++ src/main/java/com/simpleboard/board/member/application/command/MemberCommandService.java
@@ -1,47 +1,53 @@
 package com.simpleboard.board.member.application.command;
 
-import com.simpleboard.board.member.application.event.MemberUpdateNicknameEvent;
-import com.simpleboard.board.member.application.event.MemberWithdrawnEvent;
+import com.simpleboard.board.member.application.command.dto.MemberRegisterCommand;
+import com.simpleboard.board.member.application.command.event.MemberUpdateNicknameEvent;
+import com.simpleboard.board.member.application.command.event.MemberWithdrawnEvent;
 import com.simpleboard.board.member.application.exception.MemberNotFoundException;
+import com.simpleboard.board.member.domain.BirthYear;
 import com.simpleboard.board.member.domain.Member;
-import com.simpleboard.board.member.domain.NicknameDuplicationChecker;
+import com.simpleboard.board.member.domain.Nickname;
+import com.simpleboard.board.member.domain.NicknamePolicy;
 import com.simpleboard.board.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 /**
- * <p>회원 도메인에 대한 Command 유스케이스를 처리하는 Application Service.</p>
+ * 회원 도메인에 대한 Command 유스케이스를 처리하는 Application Service.
  *
  * <ul>
- *   <li>회원 탈퇴 처리</li>
- *   <li>회원 닉네임 변경 처리</li>
- *   <li>처리 후 관련 도메인 이벤트 발행</li>
+ *   <li>회원 가입 처리
+ *   <li>회원 탈퇴 처리
+ *   <li>회원 닉네임 변경 처리
+ *   <li>처리 후 관련 도메인 이벤트 발행
  * </ul>
  *
  * @domain application-service
  * @transactional
  */
 @Service
+@RequiredArgsConstructor
 public class MemberCommandService {
 
   private final MemberRepository memberRepository;
   private final ApplicationEventPublisher eventPublisher;
-  private final NicknameDuplicationChecker nicknameDuplicationChecker;
+  private final NicknamePolicy nicknamePolicy;
 
-  public MemberCommandService(
-      MemberRepository memberRepository,
-      ApplicationEventPublisher eventPublisher
-  ) {
-    this.memberRepository = memberRepository;
-    this.eventPublisher = eventPublisher;
-    this.nicknameDuplicationChecker = new NicknameDuplicationChecker(memberRepository);
+  public void registerMember(MemberRegisterCommand command) {
+    Member member =
+        Member.create(
+            command.memberId(),
+            Nickname.of(command.nickname()),
+            command.gender(),
+            BirthYear.of(command.birthYear()),
+            nicknamePolicy);
+
+    memberRepository.save(member);
   }
 
   public void withdrawMemberById(Long memberId) {
-    Member member =
-        memberRepository
-            .findById(memberId)
-            .orElseThrow(MemberNotFoundException::new);
+    Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
 
     member.withdraw();
     memberRepository.save(member);
@@ -50,13 +56,9 @@ public class MemberCommandService {
   }
 
   public void updateNickname(Long memberId, String newNickname) {
-    Member member =
-        memberRepository
-            .findById(memberId)
-            .orElseThrow(MemberNotFoundException::new);
+    Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
 
-    nicknameDuplicationChecker.checkDuplicated(newNickname);
-    member.updateNickname(newNickname);
+    member.updateNickname(Nickname.of(newNickname), nicknamePolicy);
     memberRepository.save(member);
 
     eventPublisher.publishEvent(new MemberUpdateNicknameEvent(memberId, newNickname));
