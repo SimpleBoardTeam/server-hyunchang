72ca6340cfdf0772c29e0e42a108296252a03c31
jungsu <vwx1@naver.com>
2025-08-21 17:14:46 +0900

    fix(permission): import 경로수정

--- src/main/java/com/simpleboard/board/permission/presentation/dto/DelegateRoleForm.java
+++ src/main/java/com/simpleboard/board/permission/presentation/dto/DelegateRoleForm.java
@@ -1,5 +1,5 @@
 package com.simpleboard.board.permission.presentation.dto;
 
-import com.simpleboard.board.permission.domain.RoleName;
+import com.simpleboard.board.permission.domain.vo.RoleName;
 
 public record DelegateRoleForm(Long fromUserId, String toUserNickname, RoleName roleType) {}
