0a1cb8661a503b82086bc0f998f1fa2240438d68
jungsu <vwx1@naver.com>
2025-08-21 16:46:04 +0900

    feat(member): BoardEntityRepository 및 BoardRepositoryImpl 구현

---
+++ src/main/java/com/simpleboard/board/board/infrastructure/jpa/repository/BoardRepositoryImpl.java
@@ -0,0 +1,49 @@
+package com.simpleboard.board.board.infrastructure.jpa.repository;
+
+import com.simpleboard.board.board.domain.board.entity.Board;
+import com.simpleboard.board.board.domain.board.repository.BoardRepository;
+import com.simpleboard.board.board.domain.board.vo.BoardName;
+import com.simpleboard.board.board.infrastructure.jpa.entity.BoardEntity;
+import com.simpleboard.board.board.infrastructure.jpa.mapper.BoardMapper;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BoardRepositoryImpl implements BoardRepository {
+
+  private final BoardEntityRepository boardEntityRepository;
+  private final BoardMapper boardMapper;
+
+  @Override
+  public Board save(Board board) {
+    BoardEntity toSave = boardMapper.toEntity(board);
+    BoardEntity saved = boardEntityRepository.save(toSave);
+    return boardMapper.toDomain(saved);
+  }
+
+  @Override
+  public Optional<Board> findByBoardName(String boardName) {
+    return boardEntityRepository.findByBoardName(boardName).map(boardMapper::toDomain);
+  }
+
+  @Override
+  public Optional<Board> findById(Long boardId) {
+    return boardEntityRepository.findById(boardId).map(boardMapper::toDomain);
+  }
+
+  @Override
+  public boolean existsByNameNormalized(String normalizedName) {
+    return boardEntityRepository.findAll().stream()
+        .map(BoardEntity::getBoardName)
+        .map(BoardName::of)
+        .map(BoardName::normalized)
+        .anyMatch(normalizedName::equals);
+  }
+
+  @Override
+  public void deleteById(Long boardId) {
+    boardEntityRepository.deleteById(boardId);
+  }
+}
