7f0997a3f793e7e321cf6f5bf7511586fc9427c9
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-08-24 16:51:48 +0900

    feat(auth): Jpa용 infra 엔티티 개발

---
+++ src/main/java/com/simpleboard/board/auth/infrastructure/jpa/entity/UserAuthEntity.java
@@ -0,0 +1,41 @@
+package com.simpleboard.board.auth.infrastructure.jpa.entity;
+
+import com.simpleboard.board.auth.domain.auth.vo.RegisterType;
+import com.simpleboard.board.auth.domain.auth.vo.UserState;
+import com.simpleboard.board.auth.domain.common.vo.Role;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@Inheritance(strategy = InheritanceType.JOINED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class UserAuthEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Long userId;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "user_state", nullable = false)
+  private UserState userState;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "register_type", nullable = false)
+  private RegisterType registerType;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "role", nullable = false)
+  private Role role;
+
+  public abstract String getLoginId();
+
+  public abstract String getPassword();
+}
