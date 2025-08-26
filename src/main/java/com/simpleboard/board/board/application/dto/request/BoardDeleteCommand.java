b50bc0176e2b19eda618a72b632ae17f6d6e3af7
jungsu <vwx1@naver.com>
2025-08-26 13:57:44 +0900

    docs(Board): 주석 작성

--- src/main/java/com/simpleboard/board/board/application/dto/request/BoardDeleteCommand.java
+++ src/main/java/com/simpleboard/board/board/application/dto/request/BoardDeleteCommand.java
@@ -2,5 +2,12 @@ package com.simpleboard.board.board.application.dto.request;
 
 import lombok.Builder;
 
+/**
+ * Board 삭제 요청 모델.
+ *
+ * <p>Req: Presentation -> application
+ *
+ * @domain request-dto
+ */
 @Builder
 public record BoardDeleteCommand(String boardName) {}
