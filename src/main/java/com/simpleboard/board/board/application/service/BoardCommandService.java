e505eca875a2c2bfb9e027f685f6bc1095820d07
jungsu <vwx1@naver.com>
2025-08-26 13:57:44 +0900

    docs(Board): 주석 작성

--- src/main/java/com/simpleboard/board/board/application/service/BoardCommandService.java
+++ src/main/java/com/simpleboard/board/board/application/service/BoardCommandService.java
@@ -5,6 +5,12 @@ import com.simpleboard.board.board.application.dto.request.BoardDeleteCommand;
 import com.simpleboard.board.board.application.dto.response.BoardCreateResult;
 import com.simpleboard.board.board.domain.common.vo.Visitor;
 
+/**
+ * Board Command 유스케이스.
+ *
+ * @domain application-service
+ * @transactional
+ */
 public interface BoardCommandService {
   BoardCreateResult createBoard(Visitor visitor, BoardCreateCommand command);
 
