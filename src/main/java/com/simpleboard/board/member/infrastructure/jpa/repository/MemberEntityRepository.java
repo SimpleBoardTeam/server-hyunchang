2d6a2b87cc2d72bacbca6d9aadd608c362772efe
jungsu <vwx1@naver.com>
2025-08-29 17:01:23 +0900

    feat(member): MemberEntityRepository 정의
    - existsByNicknameValue를 네이티브 쿼리로 구현

---
+++ src/main/java/com/simpleboard/board/member/infrastructure/jpa/repository/MemberEntityRepository.java
@@ -0,0 +1,13 @@
+package com.simpleboard.board.member.infrastructure.jpa.repository;
+
+import com.simpleboard.board.member.infrastructure.jpa.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberEntityRepository extends JpaRepository<MemberEntity,Long> {
+  @Query(value = "select exists (select 1 from members where nickname = :nickname)", nativeQuery = true)
+  boolean existsByNicknameValue(@Param("nickname") String nickname);
+}
