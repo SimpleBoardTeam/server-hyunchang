8098b589eb7ed8236ba6a9fed78d8c4f8565c1ef
jungsu <vwx1@naver.com>
2025-09-04 21:11:43 +0900

    chore(board): spotless 적용

--- src/main/java/com/simpleboard/board/board/infrastructure/jpa/repository/BoardEntityRepository.java
+++ src/main/java/com/simpleboard/board/board/infrastructure/jpa/repository/BoardEntityRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoardEntityRepository extends JpaRepository<BoardEntity, Long> {
   Optional<BoardEntity> findByBoardName(String boardName);
+
   boolean existsByNormalizedName(String normalizedName);
 }
